3565f9eb5fa92ddd95d47ca0e4849d3e1efcdfdb
Itay Arad <itayarad@gmail.com>
2018-03-19 14:04:15 +0200

    initial commit

--- train.py
+++ train.py
@@ -27,8 +27,8 @@ models = {
 def main(_):
     # Parsing arguments
     parser = argparse.ArgumentParser(description='Training GANs or VAEs')
-    parser.add_argument('--model', type=str, required=True)
-    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--model', type=str, required=False, default='cvaegan')
+    parser.add_argument('--dataset', type=str, required=False, default=r'.\datasets\files\celebA.hdf5')
     parser.add_argument('--datasize', type=int, default=-1)
     parser.add_argument('--epoch', type=int, default=200)
     parser.add_argument('--batchsize', type=int, default=50)
