c5c8d2928c0ffe094ee6f98298e36b319c51d725
Itay Arad <itayarad@gmail.com>
2018-03-26 10:47:04 +0300

    2nd attempt to fix gpu issue

--- models/base.py
+++ models/base.py
@@ -52,6 +52,7 @@ class HandBaseModel(metaclass=ABCMeta):
 
         config = tf.ConfigProto()
         config.gpu_options.allocator_type = 'BFC'
+        config.gpu_options.per_process_gpu_memory_fraction = 0.90
         self.sess = tf.Session(config=config)
         self.writer = None
         self.saver = None
